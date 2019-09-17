24f899483a89c1faf6160878383e527b3cef7a77
github username <github email address>
2019-09-17 17:01:09 -0400

    uploaded our attempt at the lab

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/env ruby
 
+require 'pry'
+
+
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
 character = get_character_from_user
+
 show_character_movies(character)
+
+# binding.pry
+# 0
